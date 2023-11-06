9c1f487086b9993fdf24bf0fc683cc7bb070946f
Michał Rusak <104869035+michalrusak@users.noreply.github.com>
2023-11-06 19:33:29 +0100

    feat: add all modules and angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './modules/core/core.module';
+import { QuizModule } from './modules/quiz/quiz.module';
+import { FormModule } from './modules/form/form.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CoreModule, QuizModule, FormModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
